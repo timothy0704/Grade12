34659b9eda010c5200390797e1437889cdc5ff0c
timothy0704 <114947065+timothy0704@users.noreply.github.com>
2024-11-13 17:56:49 -0500

    Create Simple_calculator.py

---
+++ Simple_calculator.py
@@ -0,0 +1,21 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter an operation (+, -, *, /): ")
+
+if operation == '+':
+        result = num1 + num2
+        print(f"The result of {num1} + {num2} is {result}")
+elif operation == '-':
+        result = num1 - num2
+        print(f"The result of {num1} - {num2} is {result}")
+elif operation == '*':
+        result = num1 * num2
+        print(f"The result of {num1} * {num2} is {result}")
+elif operation == '/':
+    if num2 != 0:
+            result = num1 / num2
+            print(f"The result of {num1} / {num2} is {result}")
+    else:
+            print("Error: Division by zero is not allowed.")
+else:
+        print("Invalid operation. Please enter one of +, -, *, or /.")
